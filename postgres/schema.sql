529a3aaac2abaf94158414ae6a1e7fbbf46f9eb9
matta13 <martin.matta@mail.udp.cl>
2025-10-01 04:00:05 -0300

    Update schema.sql

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,4 +1,3 @@
--- schema.sql
 CREATE SCHEMA IF NOT EXISTS public;
 
 CREATE TABLE IF NOT EXISTS public.querys (
@@ -7,6 +6,3 @@ CREATE TABLE IF NOT EXISTS public.querys (
   body   text,
   answer text
 );
-
--- Opcional: índice para búsquedas rápidas
--- CREATE INDEX IF NOT EXISTS idx_querys_title ON public.querys USING gin (to_tsvector('spanish', title));
